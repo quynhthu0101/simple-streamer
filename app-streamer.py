11d694d6cabc464ab1987a004c3a578e8043dc80
quynhthu0101 <163289957+quynhthu0101@users.noreply.github.com>
2025-04-23 08:52:54 +0700

    Update app-streamer.py

--- app-streamer.py
+++ app-streamer.py
@@ -23,10 +23,8 @@ with col1:
 
                           rtc_configuration={  # Add this config
                                 "iceServers": [{"urls": ["stun:stun.l.google.com:19302"],
-                                                "urls": ["stun:stun1.l.google.com:19302"],
-                                                "urls": ["stun:stun2.l.google.com:19302"],
-                                                "urls": ["stun:stun3.l.google.com:19302"],
-                                                "urls": ["stun:stun4.l.google.com:19302"]
+                                                "username": "openrelayproject",
+                                                "credential": "openrelayproject"
                                                 }]
                             },
 
